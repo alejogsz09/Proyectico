f816916e3d13a1950a712b445e1852784ccb56f0
Alejo <alejandrogarzonsanchez1@gmail.com>
2025-08-28 12:29:26 -0500

    ff

--- src/app.py
+++ src/app.py
@@ -1,6 +1,11 @@
 def suma(a, b):
     return a + b
 
+def saludo():
+    print("hola")
+    
+    
+    
 if __name__ == "__main__":
     print("Hola, Jenkins + GitHub + Slack!")
     print("Suma 2 + 3 =", suma(2, 3))
